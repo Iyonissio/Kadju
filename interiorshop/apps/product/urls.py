78a6d96bed1d4f45026e1433bc1684567f4fdeaf
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-09-19 10:36:50 +0200

    actualizar

--- interiorshop/apps/product/urls.py
+++ interiorshop/apps/product/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search/', views.search, name='search'),
+ #   path('search/', views.search, name='search'),
     path('<slug:category_slug>/<slug:product_slug>/', views.product, name='product'),
-    path('<slug:category_slug>/', views.category, name='category')
+ #   path('<slug:category_slug>/', views.category, name='category')
 ]
